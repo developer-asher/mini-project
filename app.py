8e22ae5a5dde081d4747f28f328cfbc0c26edebc
Asher <grit.developer.asher@gmail.com>
2021-09-17 09:52:32 +0900

    Add response to saved reviews

--- app.py
+++ app.py
@@ -85,8 +85,9 @@ def save_review():
     }
 
     db.reviews.insert_one(doc)
+    reviews = list(db.reviews.find({}, {'_id': False}))
 
-    return jsonify({'msg': '리뷰저장 완료!'})
+    return jsonify({'reviews': reviews})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
